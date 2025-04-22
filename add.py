a1d9cde4cfd4ddcfc4d907a9b2a84dbfd1ebe119
Ananyar2006 <155660150+Ananyar2006@users.noreply.github.com>
2025-04-22 10:13:31 +0530

    Update addition

--- add.py
+++ add.py
@@ -3,7 +3,7 @@ from app import add
 
 class TestAdd(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(3, 3), 5)
 
 if __name__ == '__main__':
     unittest.main()
